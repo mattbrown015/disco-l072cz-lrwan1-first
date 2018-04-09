f7eb160f5b57f80ae20109fe7a0136f265bac1c4
Matthew Brown <matthewb@WOKINGHAM.ENSILICA.COM>
2018-04-09 14:37:38 +0100

    Flash the green LED as further demonstration that something is happening.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <mbed.h>
 
+DigitalOut green_led(LED1);
+
 int main() {
     puts("disco-l072cz-lrwan1-first");
 
     while (1) {
         wait(2);
 
+        green_led = !green_led;
         puts("running");
     }
 }
