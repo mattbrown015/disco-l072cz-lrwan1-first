538a8954d69ef22d62b605625ec725be2215cf12
Matthew Brown <matthewb@WOKINGHAM.ENSILICA.COM>
2018-04-09 15:45:11 +0100

    Implement test using 'EventQueue'.
    The mbed OS 5 LoRaWAN stack uses the 'EventQueue' so I thought I'd try it.
    
    Unsurprisingly the ignore list needed to be reduced.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ namespace
 
 DigitalOut green_led(LED1);
 
+EventQueue event_queue;
+
 void demonstrateStillRunning() {
     green_led = !green_led;
     puts("running");
@@ -15,9 +17,8 @@ void demonstrateStillRunning() {
 int main() {
     puts("disco-l072cz-lrwan1-first");
 
-    while (1) {
-        wait(2);
+    event_queue.call_every(2000, demonstrateStillRunning);
 
-        demonstrateStillRunning();
-    }
+    puts("dispatch_forever");
+    event_queue.dispatch_forever();
 }
